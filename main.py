7a62c8155f487ab5bcd00fdf7f39f28abef606f9
Mansmor2006 <91160995+Mansmor2006@users.noreply.github.com>
2023-11-27 11:29:41 +0000

    Add horizontal and vertical lines to the screen

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import pygame
 import sys
 
+
+
+
+
 # Define some colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-GRAY = (200, 200, 200)
+
 
 # Initialize Pygame
 pygame.init()
 
-# Set up the display
-width, height = 600, 800
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Sudoku Puzzle")
 
-
+# Set up grid parameters
+rows, cols = 10, 10
 
 
 # Main game loop
 def main():
+    # Initialize Pygame
+    pygame.init()
+
+    # Set up the display
+    width, height = 600, 600
+    screen = pygame.display.set_mode((width, height))
+    screen.fill(WHITE)
+    pygame.display.set_caption("Sudoku Puzzle")
     
     while True:
         for event in pygame.event.get():
@@ -26,10 +35,14 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-      
-
-        
-
-
+        for i in range(10):
+        # Draw horizontal lines
+            cell_size = width // 9
+        # Draw horizontal lines
+            pygame.draw.line(screen, BLACK, (0, i * cell_size), (width, i * cell_size), 2)
+        # Draw vertical lines
+            pygame.draw.line(screen, BLACK, (i * cell_size, 0), (i * cell_size, height), 2)
+            
+        pygame.display.update()
 if __name__ == "__main__":
     main()
